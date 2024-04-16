858cb1927755217df0469046ad9cad5a18678b83
PlayerG9 <g9yout@gmail.com>
2024-04-16 14:16:24 +0200

    added configlib

--- src/optimizer/__main__.py
+++ src/optimizer/__main__.py
@@ -3,6 +3,7 @@ r"""
 
 """
 import argparse as ap
+import configlib
 from . import __version__, optimize
 try:
     import better_exceptions
@@ -13,10 +14,14 @@ except ModuleNotFoundError:
 
 parser = ap.ArgumentParser(prog="optimizer", formatter_class=ap.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-v', '--version', action='version', version=__version__)
+parser.add_argument('-c', '--config', default=".optimizer.yaml",
+                    help="Path to the config file")
 
 
 def main():
     args = vars(parser.parse_args())
+    configlib.config.merge(configlib.load(fp=args.pop('config')))
+    configlib.config.merge(configlib.load_environ(prefix="OPTIMIZER"))
     return optimize(**args)
 
 
